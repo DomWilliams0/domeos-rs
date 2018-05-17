b7845524bbb07173ccb6dc632b26ed8c5940fe49
Dom Williams <me@domwillia.ms>
2018-05-17 11:53:23 +0100

    Add bootloader and simple kernel

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,23 @@
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga = 0xb8000 as *const u8 as *mut u8;
+    let pos = 30;
+    unsafe {
+        *vga.offset(pos) = 'X' as u8;
+        *vga.offset(pos + 1) = 0xa;
+    }
+
     loop {}
 }
 
 #[lang = "panic_fmt"]
 #[no_mangle]
-pub extern "C" fn rust_begin_panic(_msg: core::fmt::Arguments, _file: &'static str, _line: u32, _column: u32) -> ! {
+pub extern "C" fn rust_begin_panic(
+    _msg: core::fmt::Arguments,
+    _file: &'static str,
+    _line: u32,
+    _column: u32,
+) -> ! {
     loop {}
 }
